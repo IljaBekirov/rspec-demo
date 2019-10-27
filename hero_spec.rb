6625985fd4b80dbbe20653b3a5e436d11192790d
Ilya Bekirov <ilja.bekirov@gmail.com>
2019-10-27 15:14:03 +0300

    Refactiring tests

--- hero_spec.rb
+++ hero_spec.rb
@@ -1,27 +1,21 @@
 require './hero'
 
 describe Hero do
-  it 'has a capitalize name' do
-    hero = Hero.new('mike')
+  let(:hero) { Hero.new('mike') }
 
+  it 'has a capitalize name' do
     expect(hero.name).to eq('Mike')
   end
 
   it 'can power up' do
-    hero = Hero.new('mike')
-
     expect(hero.power_up).to eq(110)
   end
 
   it 'can power down' do
-    hero = Hero.new('mike')
-
     expect(hero.power_down).to eq(90)
   end
 
   it 'Hero display full hero info' do
-    hero = Hero.new('mike')
-
     expect(hero.hero_info).to eq('Mike has a health of 100')
   end
 end
